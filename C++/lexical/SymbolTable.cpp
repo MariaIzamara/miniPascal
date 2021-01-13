ccf456258c1c978739b76625e80d3d1370b9eab8
MariaIzamara <mariaizamaracoutinho@gmail.com>
2021-01-13 08:05:02 -0300

    settings made and complementing the professor code

--- C++/lexical/SymbolTable.cpp
+++ C++/lexical/SymbolTable.cpp
@@ -1,8 +1,6 @@
 #include "SymbolTable.h"
 
 SymbolTable::SymbolTable() {
-    m_st["("] = TKN_OPEN_PAR;
-
     // SYMBOLS
     m_st["."] = TKN_DOT;
     m_st[","] = TKN_COMMA;
@@ -54,10 +52,12 @@ SymbolTable::~SymbolTable() {
 }
 
 bool SymbolTable::contains(std::string token) {
+	// se eu procurar o token e chegar no fim, então quer dizer que ele não encontrou e ai retorna falso
     return m_st.find(token) != m_st.end();
 }
 
 enum TokenType SymbolTable::find(std::string token) {
+	// consulta na tabela se existe o token, se existir consulta na tabela, senão retorna token invalido
     return this->contains(token) ?
-              m_st[token] : TKN_INVALID_TOKEN;
+        m_st[token] : TKN_INVALID_TOKEN;
 }
