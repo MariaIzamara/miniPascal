4c262a90819b2fbb67489e4085573c4ddec83288
MariaIzamara <mariaizamaracoutinho@gmail.com>
2021-03-01 23:11:47 -0300

    fix memory

--- C++/interpreter/util/Memory.cpp
+++ C++/interpreter/util/Memory.cpp
@@ -1,7 +1,7 @@
 #include "Memory.h"
 
 std::map<std::string, Type*> Memory::m_memory;
-std::set<std::string>, Memory::consts;
+std::set<std::string> Memory::m_consts;
 
 Type* Memory::read(const std::string& name) {
 	return m_memory[name];
@@ -14,4 +14,11 @@ void Memory::write(const std::string& name, Type* value) {
 		printf("Operação de sobrescrita impossível");
 }
 
+void Memory::registryVariable(const std::string& name, Type* value) {
+	write(name, value);
+}
 
+void Memory::registryConstant(const std::string& name, Type* value) {
+	write(name, value);
+	m_consts.insert(name);
+}
